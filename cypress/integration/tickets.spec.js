efa1bd4d0829748fd232d24b4b40dc4226fd0c4b
Kimberlly Amaral <kimberllys.amaral@gmail.com>
2021-11-21 16:03:31 -0300

    test: interacting with a select field

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -2,7 +2,7 @@ describe("Tickets", () => {
     // visiting the following url
     beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html")) 
     // .only can be added if you want to run just this specific test;
-    it.only("fills all the text input fields", () =>{
+    it("fills all the text input fields", () =>{
         const firstName = "Kimberlly";
         const lastName = "Amaral";
 
@@ -11,6 +11,10 @@ describe("Tickets", () => {
         cy.get("#email").type("kimberllys.amaral@gmail.com");
         cy.get("#requests").type("Vegetarian");
         cy.get("#signature").type(`${firstName} ${lastName}`);
+    });
+
+    it.only("select two tickets", () => {
+        cy.get("#ticket-quantity").select("2");
     });
     // Checking if the header has the title 'TICKETBOX'
     it("has 'TICKETBOX' header's heading", () => {});
